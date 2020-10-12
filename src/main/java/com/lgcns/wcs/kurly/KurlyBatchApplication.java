55dd49b7bc88af7c5ceea4f5f7272e3f655e4b3a
jooninara <67253381+jooninara@users.noreply.github.com>
2020-10-12 11:37:51 +0900

    init

---
+++ src/main/java/com/lgcns/wcs/kurly/KurlyBatchApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.wcs.kurly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurlyBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurlyBatchApplication.class, args);
+	}
+
+}
