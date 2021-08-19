986f3f0036d19f0e87757f65cea1413b700b59d5
yeonggeolgho <yeonggeol.gho@kurlycorp.com>
2021-08-19 14:57:53 +0900

    수정: java parametes & datadog setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-java -Xms2048m -Xmx2048m -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseStringDeduplication -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m -verbose:gc -XX:+PrintGCDetails -jar -Dspring.profiles.active=$RUN_ENV -Duser.timezone=Asia/Seoul $RUN_JVM_PARAM /usr/local/bin/kurlyBatch-0.0.1-SNAPSHOT.jar
+
+#java -Xms2048m -Xmx2048m -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseStringDeduplication -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m -verbose:gc -XX:+PrintGCDetails -jar -Dspring.profiles.active=$RUN_ENV -Duser.timezone=Asia/Seoul $RUN_JVM_PARAM /usr/local/bin/kurlyBatch-0.0.1-SNAPSHOT.jar
+java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=$RUN_ENV $RUN_JVM_PARAM $JAVA_AGENT /usr/local/bin/kurlyBatch-0.0.1-SNAPSHOT.jar
