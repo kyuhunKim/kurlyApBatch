f214d5d6d940a58f2f7e89be3c9cde6a2e0daeaf
baehwan <hwan.bae@kurlycorp.com>
2022-05-24 19:38:18 +0900

    [WCS-AP-BATCH] 김포 DAS 추가 개발 내역
    주문 수정정보 재전송 - WCS-DAS-API 전송(신규토픽)

--- src/main/java/com/lgcns/wcs/kurly/ScheduleTask.java
+++ src/main/java/com/lgcns/wcs/kurly/ScheduleTask.java
@@ -60,7 +60,7 @@ public class ScheduleTask {
 	@Scheduled(fixedDelay = 600000)
 	public void testTask() {
 //		log.info("The current date (1) testTask: " + LocalDateTime.now());
-//		String resDate = boxRecom.selectDate();
+		String resDate = boxRecom.selectDate();
 
 //		log.info("Current Thread : {}", Thread.currentThread().getName());
 	}
@@ -69,94 +69,94 @@ public class ScheduleTask {
 	public void BoxRecom() {
 //		log.info("The current date (2) BoxRecom : " + LocalDateTime.now());
 //		log.info("Current Thread : {}", Thread.currentThread().getName());
-//		boxRecom.BoxRecomBatchTask();
+		boxRecom.BoxRecomBatchTask();
 	}
 	//토트 마스터 초기화(Release) 정보 연계 - 10초
 	@Scheduled(fixedDelay = 10000)
 	public void ToteRelease() {
 //		System.out.println("The current date (3) ToteRelease : " + LocalDateTime.now());
-//		tote.ToteReleaseTask();
+		tote.ToteReleaseTask();
 	}
 	//분류 권역 정보  - 확인필요
 	@Scheduled(fixedDelay = 3600000)
 	public void RegionMasterTask() {
 //		System.out.println("The current date (4) RegionMasterTask : " + LocalDateTime.now());
-//		regionMaster.RegionMasterTask();
+		regionMaster.RegionMasterTask();
 	}
 	//WCS 토트 자동화 설비 투입 정보 (토트 기준) - 10초
 	@Scheduled(fixedDelay = 10000)
 	public void ToteScan() {
 //		System.out.println("The current date (5) ToteRelease : " + LocalDateTime.now());
-//		toteScan.ToteScanTask();
+		toteScan.ToteScanTask();
 	}
 	//WCS 문제처리용 피킹정보 연계 - 10초
 	@Scheduled(fixedDelay = 10000)
 	public void ToteCellExceptTxn() {
 //		System.out.println("The current date (6) ToteRelease : " + LocalDateTime.now());
-//		toteCellExceptTxn.ToteCellExceptTxnTask();
+		toteCellExceptTxn.ToteCellExceptTxnTask();
 	}
 	//WCS 미출오더 처리용 WMS 피킹지시금지 정보 연계 - 10초
 	@Scheduled(fixedDelay = 10000)
 	public void OrdmadeNotfully() {
 //		System.out.println("The current date (7) ToteRelease : " + LocalDateTime.now());
-//		ordmadeNotfully.OrdmadeNotfullyTask();
+		ordmadeNotfully.OrdmadeNotfullyTask();
 	}
 	
 	//WCS 미출오더 상품보충용 추가피킹정보 연계 - 10초
 	@Scheduled(fixedDelay = 10000)
 	public void OrdmadeNotfullyReplay() {
 //		System.out.println("The current date (8) ToteRelease : " + LocalDateTime.now());
-//		ordmadeNotfullyReplay.OrdmadeNotfullyReplayTask();
+		ordmadeNotfullyReplay.OrdmadeNotfullyReplayTask();
 	}
 	//WCS 오더 피킹 완료 정보 - 1분
 	@Scheduled(fixedDelay = 60000)
 	public void PickQpsComplet() {
 //		System.out.println("The current date (9) PickQpsComplet : " + LocalDateTime.now());
-//		pickQpsComplet.PickQpsCompletTask();
+		pickQpsComplet.PickQpsCompletTask();
 	}
 	//WCS 오더 패킹 완료 정보 - 1분
 	@Scheduled(fixedDelay = 60000)
 	public void PackQpsComplet() {
 //		System.out.println("The current date (10) PackQpsComplet : " + LocalDateTime.now());
-//		 packQpsComplet.PackQpsCompletTask();
+		 packQpsComplet.PackQpsCompletTask();
 	}
 	
 	//WCS 운송장 발행 정보 - 1분
 	@Scheduled(fixedDelay = 60000)
 	public void InvoicePrintComplet() {
 //		System.out.println("The current date (11) InvoicePrintComplet : " + LocalDateTime.now());
-//		invoicePrintComplet.InvoicePrintCompletTask();
+		invoicePrintComplet.InvoicePrintCompletTask();
 	}
 	//WCS 방면 분류 완료 정보 - 1분
 	@Scheduled(fixedDelay = 60000) 
 	public void InvoiceSortComplet() {
 //		System.out.println("The current date (12) InvoiceSortComplet : " + LocalDateTime.now());
-//		invoiceSortComplet.InvoiceSortCompletTask();
+		invoiceSortComplet.InvoiceSortCompletTask();
 	}
 	//QPS 호기별 가용셀 정보 - 10초
 	@Scheduled(fixedDelay = 10000) 
 	public void QpsNumUseCell() {
 //		System.out.println("The current date (13) QpsNumUseCell : " + LocalDateTime.now());
-//		qpsNumUseCell.QpsNumUseCellTask();
+		qpsNumUseCell.QpsNumUseCellTask();
 	}
 	//DAS 그룹번호별 가용셀 정보 - 10초
 	@Scheduled(fixedDelay = 10000) 
 	public void DasNumUseCell() {
 //		System.out.println("The current date (14) DasNumUseCell : " + LocalDateTime.now());
-//		dasNumUseCell.DasNumUseCellTask();
+		dasNumUseCell.DasNumUseCellTask();
 	}
 
 	//최적화 배치 미처리분 재작업 - 3분
 	@Scheduled(fixedDelay = 180000) 
 	public void ReplayOptimizBatch() {
 //		System.out.println("The current date (15) InvoiceSortComplet : " + LocalDateTime.now());
-//		replayOptimizBatch.ReplayOptimizBatchTask();
+		replayOptimizBatch.ReplayOptimizBatchTask();
 	}
 
 	//최적화 주문정보 WCS-DAS_API로 전송 - 30초
 	@Scheduled(fixedDelay = 30000)
 	public void workBatchOrderToDas() {
-//		wcsToDasBatch.workBatchOrderInfoTask();
+		wcsToDasBatch.workBatchOrderInfoTask();
 	}
 
 	//최적화 주문 수정 정보 WCS-DAS_API로 전송 - 35초
@@ -168,12 +168,12 @@ public class ScheduleTask {
 	//피킹완료 토트정보 WCS-DAS_API로 전송 - 40초
 	@Scheduled(fixedDelay = 45000)
 	public void pickingCompleteToteToDas(){
-//		wcsToDasBatch.pickingCompleteToteInfoTask();
+		wcsToDasBatch.pickingCompleteToteInfoTask();
 	}
 
 	//피킹완료후 취소 토트정보 WCS-DAS_API로 전송 - 55초
 	@Scheduled(fixedDelay = 55000)
 	public void pickingCnclToteToDas(){
-//		wcsToDasBatch.pickingCnclToteInfoTask();
+		wcsToDasBatch.pickingCnclToteInfoTask();
 	}
 }
